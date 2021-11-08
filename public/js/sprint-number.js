3f03935189adb0f54cd0536cee6d9b8734682235
Maxime RAUCH <maxime@rauch.fr>
2021-11-09 00:02:52 +0100

    fix(power-up): update form fields

--- public/js/sprint-number.js
+++ public/js/sprint-number.js
@@ -3,11 +3,11 @@
 var t = TrelloPowerUp.iframe();
 
 // Elements with IDs are available as properties of `window`.
-window.estimate.addEventListener("submit", function (event) {
+window.sprintNumberForm.addEventListener("submit", function (event) {
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
   return t
-    .set("card", "shared", "sprintNumber", window.sprintNumber.value)
+    .set("card", "shared", "sprintNumber", window.sprintNumberSelect.value)
     .then(function () {
       t.closePopup();
     });
@@ -17,9 +17,9 @@ t.render(function () {
   return t
     .get("card", "shared", "sprintNumber")
     .then(function (sprintNumber) {
-      window.sprintNumber.value = sprintNumber;
+      window.sprintNumberSelect.value = sprintNumber;
     })
     .then(function () {
-      t.sizeTo("#sprint-number").done();
+      t.sizeTo("#sprintNumberForm");
     });
 });
