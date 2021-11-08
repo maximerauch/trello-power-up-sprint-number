dac0985d75cc8a3fc66b08385666a7837c681681
Maxime RAUCH <maxime@rauch.fr>
2021-11-09 00:35:02 +0100

    fix(power-up): add custom icons

--- public/js/client.js
+++ public/js/client.js
@@ -2,14 +2,11 @@
 
 var Promise = TrelloPowerUp.Promise;
 
-var BLACK_ROCKET_ICON =
-  "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421";
-
 TrelloPowerUp.initialize({
   "card-buttons": function (t, options) {
     return [
       {
-        icon: BLACK_ROCKET_ICON,
+        icon: "images/icon_sprint.png",
         text: "Sprint Number",
         callback: function (t) {
           return t.popup({
@@ -19,5 +16,18 @@ TrelloPowerUp.initialize({
         },
       },
     ];
+  },
+
+  "card-badges": function (t, options) {
+    return t
+      .get("card", "shared", "sprintNumber")
+      .then(function (sprintNumber) {
+        return [
+          {
+            icon: "images/icon_sprint.png",
+            text: sprintNumber,
+          },
+        ];
+      });
   },
 });
