9fc9209e792b188ff6c25a4f099bbd5c4006bc6c
Maxime RAUCH <maxime@rauch.fr>
2021-11-08 23:09:18 +0100

    chore(power-up): initialize project

---
+++ public/js/sprint-number.js
@@ -0,0 +1,25 @@
+/* global TrelloPowerUp */
+
+var t = TrelloPowerUp.iframe();
+
+// Elements with IDs are available as properties of `window`.
+window.estimate.addEventListener("submit", function (event) {
+  // Stop the browser trying to submit the form itself.
+  event.preventDefault();
+  return t
+    .set("card", "shared", "sprintNumber", window.sprintNumber.value)
+    .then(function () {
+      t.closePopup();
+    });
+});
+
+t.render(function () {
+  return t
+    .get("card", "shared", "sprintNumber")
+    .then(function (sprintNumber) {
+      window.sprintNumber.value = sprintNumber;
+    })
+    .then(function () {
+      t.sizeTo("#sprint-number");
+    });
+});
