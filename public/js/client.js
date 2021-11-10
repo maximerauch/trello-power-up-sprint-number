203dbdec5a8e9fc59e21262674b2c5b2674c1656
Maxime RAUCH <maxime@rauch.fr>
2021-11-11 00:16:02 +0100

    fix(power-up): update display

--- public/js/client.js
+++ public/js/client.js
@@ -6,6 +6,25 @@ var BLACK_SPRINT_ICON =
   "https://trello-power-up-sprint-number.herokuapp.com/images/icon_sprint.png";
 
 TrelloPowerUp.initialize({
+  "board-buttons": function (t, opts) {
+    return [
+      {
+        icon: {
+          dark: BLACK_SPRINT_ICON,
+          light: BLACK_SPRINT_ICON,
+        },
+        text: "Recherchée avancée",
+        condition: "always",
+        callback: function (t) {
+          return t.popup({
+            title: "Définir le n° du sprint",
+            url: "views/sprint-number.html",
+          });
+        },
+      },
+    ];
+  },
+
   "card-buttons": function (t, options) {
     return [
       {
@@ -48,7 +67,7 @@ TrelloPowerUp.initialize({
         return [
           {
             title: "Sprint",
-            text: sprintNumber ? ("0" + sprintNumber).slice(-2) : "Aucun",
+            text: sprintNumber || "Aucun",
             color: getColorByNumber(sprintNumber),
             callback: function (t) {
               return t.popup({
@@ -69,47 +88,33 @@ function getColorByNumber(sprintNumber) {
 
   switch (sprintNumber) {
     case 1:
-      console.log("red");
       return "red";
 
     case 2:
-      console.log("yellow");
       return "yellow";
 
     case 3:
-      console.log("orange");
       return "orange";
 
     case 4:
-      console.log("red");
       return "red";
 
     case 5:
-      console.log("purple");
       return "purple";
 
     case 6:
-      console.log("blue");
       return "blue";
 
     case 7:
-      console.log("sky");
       return "sky";
 
     case 8:
-      console.log("lime");
       return "lime";
 
     case 9:
-      console.log("pink");
       return "pink";
 
-    case 10:
-      console.log("black");
-      return "black";
-
     default:
-      console.log("null");
       return null;
   }
 }
