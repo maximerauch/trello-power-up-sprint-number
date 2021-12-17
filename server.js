e54f45c0b0712e4fee9a5747be14200c33c9d05c
Maxime RAUCH <maxime@rauch.fr>
2021-12-17 18:46:03 +0100

    fix(power-up): split power-ups

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var app = express();
 
 app.use(express.static("public"));
 
-app.get("*", function (request, response) {
-  response.sendFile(__dirname + "/public/views/index.html");
+app.get("/:name", function (request, response) {
+  response.sendFile(
+    __dirname + "/" + request.params.name + "/views/index.html"
+  );
 });
 
 var listener = app.listen(process.env.PORT, function () {
